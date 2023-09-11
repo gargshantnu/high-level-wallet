4b89fdd2a9ca47f20fd776cc83126ac56b5059ca
Shantnu Garg <gargshantnu@gmail.com>
2023-09-11 12:15:38 +0530

    update port

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use("/home", homeRoutes);
 app.use("/", walletRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
